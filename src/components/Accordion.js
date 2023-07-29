007d8d89db7e950565887c696a2302d263ebb099
Kolawole Daniel <kolawolecoke@gmail.com>
2023-07-29 20:52:50 +0100

    Accordion component

---
+++ src/components/Accordion.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
+const Accordion = ({ items }) => {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleClick = (index) => {
+    if (index === expandedIndex) {
+      setExpandedIndex(-1);
+    } else {
+      setExpandedIndex(index);
+    }
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = expandedIndex === index;
+
+    return (
+      <div key={item.id}>
+        <div
+          onClick={() => handleClick(index)}
+          className="flex p-3 bg-gray-50 cursor-pointer items-center justify-between"
+        >
+          {item.title}
+          <span className="text-2xl">
+            {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+          </span>
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
+      </div>
+    );
+  });
+
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
+};
+
+export default Accordion;
---
+++ src/screens/ButtonPage.js
@@ -0,0 +1,7 @@
+
+
+const ButtonPage = () => {
+    return <div></div>;
+  };
+  
+  export default ButtonPage;

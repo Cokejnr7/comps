fc2d28e7a6c9846e3be426571a8aa5bfa62b680a
Kolawole Daniel <kolawolecoke@gmail.com>
2023-08-01 08:12:40 +0100

    code refactor and adding style to DropDown component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import DropDown from "./components/DropDown";
 
 const App = () => {
+  const [selected, setSelected] = useState(null);
+
   const options = [
     { label: "Red", value: "red" },
     { label: "Green", value: "green" },
     { label: "Blue", value: "blue" },
   ];
 
+  const handleSelect = (option) => {
+    setSelected(option);
+  };
+
   return (
     <div className="app">
-      <DropDown options={options} />
+      <DropDown options={options} value={selected} onChange={handleSelect} />
     </div>
   );
 };
--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
 
-const DropDown = ({ options }) => {
+const DropDown = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState("");
 
   const handleClick = () => {
     setIsOpen((currentIsOpen) => !currentIsOpen);
@@ -10,20 +10,32 @@ const DropDown = ({ options }) => {
 
   const handleOptionClick = (option) => {
     setIsOpen(false);
+    onChange(option);
   };
 
   const renderedOptions = options.map((option) => {
     return (
-      <div key={option.value} onClick={() => handleOptionClick(option)}>
+      <div
+        key={option.value}
+        onClick={() => handleOptionClick(option)}
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+      >
         {option.label}
       </div>
     );
   });
 
   return (
-    <div onClick={handleClick}>
-      <div>select...</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div onClick={handleClick} className="w-48 relative">
+      <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full">
+        {value?.label || "select..."}
+        <GoChevronDown className="text-lg" />
+      </div>
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
     </div>
   );
 };

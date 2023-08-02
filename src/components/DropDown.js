645af0343ddae927a8225c6af00cf21950922e72
Kolawole Daniel <kolawolecoke@gmail.com>
2023-08-02 07:28:54 +0100

    close dropdown on click of other elements

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { GoChevronDown } from "react-icons/go";
 import Panel from "./Panel";
 
 const DropDown = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (e) => {
+      // if there is no refrence exit the function
+      if (!divEl.current) return;
+
+      //checks if the click was not on the dropdown
+      if (!divEl.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handler, true);
+    return () => {
+      document.removeEventListener("click".handler);
+    };
+  }, []);
 
   const handleClick = () => {
     setIsOpen((currentIsOpen) => !currentIsOpen);
@@ -27,7 +45,7 @@ const DropDown = ({ options, value, onChange }) => {
   });
 
   return (
-    <div className="w-48 relative">
+    <div className="w-48 relative" ref={divEl}>
       <Panel
         onClick={handleClick}
         className="flex justify-between items-center cursor-pointer "
@@ -35,7 +53,9 @@ const DropDown = ({ options, value, onChange }) => {
         {value?.label || "select..."}
         <GoChevronDown className="text-lg" />
       </Panel>
-      {isOpen && <Panel className="absolute top-fulls">{renderedOptions}</Panel>}
+      {isOpen && (
+        <Panel className="absolute top-fulls">{renderedOptions}</Panel>
+      )}
     </div>
   );
 };

0528e0b48b1fddac5c347cd3e4a067263db022f5
Kolawole Daniel <kolawolecoke@gmail.com>
2023-08-04 06:04:25 +0100

    finished the modal component

--- src/screens/ModalPage.js
+++ src/screens/ModalPage.js
@@ -1,10 +1,36 @@
+import { useState } from "react";
+import Button from "../components/Button";
 import Modal from "../components/Modal";
 
 const ModalPage = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <Button primary onClick={handleClose}>
+      I Accept
+    </Button>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      Here is an Important agreement for you to accept
+    </Modal>
+  );
+
   return (
     <div>
-      <Modal />
-      
+      <Button onClick={handleClick} primary>
+        Open Modal
+      </Button>
+      {showModal && modal}
     </div>
   );
 };

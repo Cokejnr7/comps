d665ae1cf7be9e3a8a86aaf954c8832b5a0d170e
Kolawole Daniel <kolawolecoke@gmail.com>
2023-08-05 19:22:38 +0100

    making the table reusable

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,31 @@
-const Table = () => {
-  return <table>
-    
-  </table>;
+const Table = ({ data, config }) => {
+  const renderedRows = data.map((rowData) => {
+    const renderedRow = config.map((row) => {
+      return (
+        <td className="p-3" key={row.label}>
+          {row.render(rowData)}
+        </td>
+      );
+    });
+    return (
+      <tr className="border-b" key={rowData.name}>
+        {renderedRow}
+      </tr>
+    );
+  });
+
+  const renderedColumns = config.map((col) => {
+    return <th key={col.label}>{col.label}</th>;
+  });
+
+  return (
+    <table className="border-seperate border-spacing-2 table-auto">
+      <thead>
+        <tr className="border-b-2">{renderedColumns}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 };
 
 export default Table;
--- src/screens/TablePage.js
+++ src/screens/TablePage.js
@@ -1,5 +1,49 @@
+import Table from "../components/Table";
+
 const TablePage = () => {
-  return <div></div>;
+  const config = [
+    {
+      label: "Fruits",
+      render: (fruit) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+    },
+  ];
+
+  const data = [
+    {
+      name: "Orange",
+      color: "bg-orange-500",
+      score: 5,
+    },
+    {
+      name: "Apple",
+      color: "bg-red-500",
+      score: 2,
+    },
+    {
+      name: "Lemon",
+      color: "bg-green-500",
+      score: 8,
+    },
+    {
+      name: "Banana",
+      color: "bg-yellow-500",
+      score: 5,
+    },
+  ];
+
+  return (
+    <div>
+      <Table data={data} config={config} />
+    </div>
+  );
 };
 
 export default TablePage;
